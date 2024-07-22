2e852aeb80302fa4fa9d0776578fd862f4ebd8fb
Farah <farahzalsafwan@gmail.com>
2024-07-22 10:01:41 +0300

    fixed unused import

--- app/common/database/database.py
+++ app/common/database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker , declarative_base
 from config import DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE
 
 # Fetch database URL from environment variables
-DATABASE_URL = "postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
+DATABASE_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
 
 
 engine = create_engine(DATABASE_URL)
